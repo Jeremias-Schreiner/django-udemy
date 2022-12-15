6bf81f67d73369ccf662607094f77193a598a8fb
Jeremias Schreiner <jeremias-ush@hotmail.com>
2022-12-15 15:04:12 -0300

    Primer template y view

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-^jq&@kjfj+y5a+ej^$6qzk75k+!7+uu+zadr6e^o+##ubs_a$-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
