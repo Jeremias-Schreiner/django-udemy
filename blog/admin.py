60d7daa78a5967971f3200bfeace4d89c7775449
Jeremias Schreiner <jeremias-ush@hotmail.com>
2022-12-15 14:22:35 -0300

    configuracion del admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'modifield')
+    list_display =('title', 'created', 'modifield')
+    date_hierarchy = 'created'
+    list_per_page = 10
+
 
 # Register your models here.
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,14 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
-    modifield = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=200, verbose_name="Titulo")
+    content = models.TextField(verbose_name="Contenido")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Creado")
+    modifield = models.DateTimeField(auto_now=True, verbose_name="Modificado")
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name="Entrada"
+        verbose_name_plural="Entradas"
