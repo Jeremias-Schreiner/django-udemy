506f8473c5c109820e659d6f58bf30c8b1c66bdd
Jeremias Schreiner <jeremias-ush@hotmail.com>
2022-12-15 15:15:07 -0300

    Usando el contexto en home

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
+from .models import Post
 
 # Create your views here.
 
 def home(request):
-    return render(request, "blog/home.html")
+    posts = Post.objects.all()
+    return render(request, "blog/home.html", context={'posts':posts})
