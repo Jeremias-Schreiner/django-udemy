114d6168d5507f3c8d6e2d936e41864d601a2611
Jeremias Schreiner <jeremias-ush@hotmail.com>
2022-12-15 13:15:03 -0300

    Clase Modelos en Django

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    modifield = models.DateTimeField(auto_now=True)
